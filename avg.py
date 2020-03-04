109c11a7bda28c460be4d817066adcab71fd5f98
Jay Yuan <hayua7435@gmail.com>
2020-03-04 04:32:31 -0500

    added description

--- avg.py
+++ avg.py
@@ -2,4 +2,4 @@
 def average(numbers):
 	return sum(numbers) / len(numbers)
 	
-print(average(list(map(int, input('Please entre numbers:').split(' ')))))
+print(average(list(map(int, input('Please entre numbers, please separate with space: ').split(' ')))))
